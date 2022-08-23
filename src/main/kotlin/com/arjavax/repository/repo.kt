3a40056874b149f7686360a56dbdbf99d9fc3bc7
arjava <arjava379@gmail.com>
2022-08-23 17:47:48 +0700

    add Note Table and Crud in Repo

--- src/main/kotlin/com/arjavax/repository/repo.kt
+++ src/main/kotlin/com/arjavax/repository/repo.kt
@@ -1,21 +1,21 @@
 package com.arjavax.repository
 
+import com.arjavax.data.model.Note
 import com.arjavax.data.model.User
+import com.arjavax.data.table.NoteTable
 import com.arjavax.data.table.UserTable
 import com.arjavax.repository.DatabaseFactory.dbQuery
-import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
 
 class Repo {
 
     suspend fun addUser(user: User){
         dbQuery{
             UserTable.insert {
-                it[UserTable.email] = user.email
-                it[UserTable.name] = user.userName
-                it[UserTable.hashPassword] = user.hashPassword
+                it[email] = user.email
+                it[name] = user.userName
+                it[hashPassword] = user.hashPassword
             }
         }
     }
@@ -34,6 +34,58 @@ class Repo {
             email = row[UserTable.email],
             hashPassword = row[UserTable.hashPassword],
             userName = row[UserTable.name]
+        )
+    }
+
+    suspend fun addNote(note: Note, email: String){
+        dbQuery {
+            NoteTable.insert {
+                it[id] = note.id
+                it[noteTitle] = note.noteTitle
+                it[userEmail] = email
+                it[description] = note.description
+                it[date] = note.date
+            }
+        }
+    }
+
+    suspend fun getAllNotes(email: String):List<Note> = dbQuery {
+        NoteTable.select {
+            NoteTable.userEmail.eq(email)
+        }.mapNotNull { rowToNote(it) }
+    }
+
+    suspend fun updateNote(note: Note, email: String){
+        dbQuery {
+            NoteTable.update(
+                where = {
+                    NoteTable.userEmail.eq(email) and NoteTable.id.eq(note.id)
+                }
+            ){
+                it[NoteTable.noteTitle] = note.noteTitle
+                it[description] = note.description
+                it[date] = note.date
+            }
+        }
+    }
+
+    suspend fun deleteNote(id: String){
+        dbQuery {
+            NoteTable.deleteWhere {
+                NoteTable.id.eq(id)
+            }
+        }
+    }
+
+    private fun rowToNote(row: ResultRow?):Note?{
+        if (row == null){
+            return null
+        }
+        return Note(
+            id = row[NoteTable.id],
+            noteTitle = row[NoteTable.noteTitle],
+            description = row[NoteTable.description],
+            date = row[NoteTable.date]
         )
     }
 }
